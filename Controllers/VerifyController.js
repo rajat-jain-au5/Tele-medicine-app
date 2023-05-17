94bc7332b2379521dfcb39fe4b3078427f0941e7
rajat <rajat@edsanta.com>
2023-05-17 21:12:50 +0530

    add

--- Controllers/VerifyController.js
+++ Controllers/VerifyController.js
@@ -4,51 +4,51 @@ const jwt = require('jsonwebtoken')
 const verifyController = {}
 
 
-verifyController.verify = function(req,res){
+verifyController.verify = function (req, res) {
 
   var userEmail = req.body.email
   var userInfo = req.body.userinfo
   jwt.sign(
-       req.body,
-      "amit",
-      { expiresIn : 600},
-     async (err, token) => {
-          
-          if(err) throw err;
-        
-            //step 1
-  let transpoter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth : {
-        user : "medicalapp331@gmail.com",
-        pass : "mjpdksnlvndgyhwe"
-      }
-    })
+    req.body,
+    "amit",
+    { expiresIn: 600 },
+    async (err, token) => {
 
-    //step 2
-    let mailOptions ={
-      from : "medicalapp331@gmail.com",
-      to : userEmail,
-      subject : "Med - Tech ",
-      text: "IT works",
-      html:
-        "Welcome to Med-Tech.Please click on Link to register Your account.<br><a href=https://arogya-app.onrender.com/" + userInfo + "?token=" + token +" target='_blank'>https://arogya-app.onrender.com/doc?token="+token+"</a>"  
+      if (err) throw err;
+
+      //step 1
+      let transpoter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: "medicalapp331@gmail.com",
+          pass: "mjpdksnlvndgyhwe"
+        }
+      })
+
+      //step 2
+      let mailOptions = {
+        from: "medicalapp331@gmail.com",
+        to: userEmail,
+        subject: "Med - Tech ",
+        text: "IT works",
+        html:
+          "Welcome to Med-Tech.Please click on Link to register Your account.<br><a href=https://arogya-api.onrender.com/" + userInfo + "?token=" + token + " target='_blank'>https://arogya-api.onrender.com/" + userInfo + "?token=" + token + "</a>"
+      }
+
+      await transpoter.sendMail(mailOptions, function (err, userData) {
+        if (err) {
+          console.log("error occurs", err)
+        } else {
+
+        }
+      })
     }
-
-  await  transpoter.sendMail(mailOptions, function(err, userData) {
-      if(err){
-        console.log("error occurs",err)
-      }else{
-    
-      }
-    })
-}
   )
 
 
-   res.send("Please verify account")
+  res.send("Please verify account")
 
 }
 
